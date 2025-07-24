c267c7965738e94830e6de6f357d7ada588ab077
Valerie Freeman <valerah7@gmail.com>
2025-07-24 13:09:10 -0500

    skeleton created

---
+++ src/chapters/workshop3/docker-compose/intro-to-docker-compose.js
@@ -0,0 +1,10 @@
+export const workshop3IntroToDCChapter = {
+  id: "workshop3-intro-to-docker-compose",
+  title: "Intro to Docker Compose",
+  sectionId: "docker-compose",
+  previousChapterId: null,
+  content: `
+  Content TBD
+`,
+  exercise: null,
+}
